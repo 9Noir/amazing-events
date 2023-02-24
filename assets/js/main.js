99be092b868d50e1ee15a5b8605d9fec653fc344
Noir <jat_9000@hotmail.com>
2023-02-24 04:00:07 -0300

    Added category filter and search text functions, and implemented rendering of event cards  based on the current page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const darkModeBtn = document.getElementById("toggleDarkButton");
+const darkModeBtn = document.querySelector(".toggleDark-btn");
 checkTheme();
 
 function checkTheme() {
@@ -16,4 +16,66 @@ function toggleDarkMode() {
     document.documentElement.classList.toggle("dark") ? (localStorage.theme = "dark") : (localStorage.theme = "light");
     darkModeBtn.classList.toggle("bi-moon");
     darkModeBtn.classList.toggle("bi-moon-fill");
+}
+
+// let category = [];
+// data.events.forEach((e) => {
+//     !category.includes(e.category) ? category.push(e.category) : undefined;
+// });
+
+// let categoryContainer = document.querySelector(".category-container");
+// category.forEach((c) => (categoryContainer.innerHTML += `<input type="checkbox" id="${c}" name="category" value="${c}" onChange="filterEvents()"><label for="${c}">${c}</label>`));
+
+let events;
+let path = location.pathname;
+let pathCards = ["index","upcoming","past"];
+if (path.includes(pathCards[0])) {
+    events = data.events;
+} else if (path.includes(pathCards[1])) {
+    events = data.events.filter((e) => new Date(data.currentDate) < new Date(e.date));
+} else if (path.includes(pathCards[2])) {
+    events = data.events.filter((e) => new Date(data.currentDate) > new Date(e.date));
+}
+
+//Para renderizar solo en las pathCards
+if(pathCards.some(p=>path.includes(p))){
+    var eventsContainer = document.querySelector(".events-container");
+    //Render de todos los eventos
+    renderCards(events);
+}
+
+function renderCards(events) {
+    eventsContainer.innerHTML = "";
+    events.forEach((e) => {
+        let card = document.querySelector(".event-container").cloneNode(true);
+        card.classList.remove("hidden");
+        (new Date(data.currentDate) > new Date(e.date)) && path.includes("index") ? card.classList.add("grayscale") : undefined;
+        card.querySelector(".event-category").textContent = e.category;
+        card.querySelector(".event-img").src = e.image;
+        card.querySelector(".event-title").textContent = e.name;
+        card.querySelector(".event-description").textContent = e.description;
+        card.querySelector(".event-assistance").innerHTML += e.assistance;
+        card.querySelector(".event-date").innerHTML += e.date;
+        card.querySelector(".event-price").innerHTML += e.price;
+        card.querySelector(".event-place").innerHTML += e.place;
+        eventsContainer.appendChild(card);
+    });
+}
+
+function filterEvents() {
+    let filteredEvents = [];
+    let checkedCategories = [];
+    let searchText = document.querySelector('input[type="search"]').value;
+
+    //Captura de todos los input checked y agregados al array checkedCategories
+    document.querySelectorAll('input[name="category"]:checked').forEach((c) => checkedCategories.push(c.value));
+
+    //Comprobación para cada evento(e) si su categoria esta incluida en checkedCategories o si no se capturo ningun checked
+    events.forEach((e) => {
+        let categoryMatch = checkedCategories.includes(e.category) || checkedCategories.length === 0;
+        let searchMatch = e.name.toLowerCase().includes(searchText.toLowerCase());
+        if (!categoryMatch || !searchMatch) return;
+        filteredEvents.push(e); //Si no hubo match en categoria o en search, se sigue con el siguiente, sino se agrega a filteredEvents
+    });
+    renderCards(filteredEvents); //Render de cards con los eventos filtrados
 }
